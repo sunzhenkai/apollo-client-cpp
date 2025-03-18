46556acd38bc76597019c770bc69557ef8a88236
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-18 11:02:02 +0800

    stage

--- include/apollo/client.h
+++ include/apollo/client.h
@@ -1,9 +1,32 @@
+/**
+ * @file client.h
+ * @brief apollo client
+ * @author zhenkai.sun
+ * */
 #include <string>
+#include <unordered_map>
+// self
+#include "apollo/model.h"
 
 namespace apollo {
-struct ApolloClientOptions {
-  std::string address; // apollo config server address with port
-};
+class ApolloClient {
+public:
+  ApolloClient(ApolloClientOptions &&options); //< Construct
+  ~ApolloClient();                             //< Destructor
 
-class ApolloClient {};
+  /**
+   * @brief Get all properties of the namespace (with cache)
+   * @param [in] nmspace namespace
+   * @param [in] ttl_s maximum allowable delay time in seconds, the value 0
+   * means query
+   * @return the properties of the namespace
+   * immediately
+   */
+  Properties GetProperties(const std::string &nmspace, int ttl_s = 60);
+
+  void Subscribe(SubscribeMeta &&meta);
+
+private:
+  ApolloClientOptions options;
+};
 } // namespace apollo
--- include/apollo/model.h
+++ include/apollo/model.h
@@ -1,11 +1,34 @@
+/**
+ * @file model.h
+ * @brief the defination of models
+ * @author zhenkai.sun
+ */
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace apollo {
+using Properties = std::unordered_map<std::string, std::string>;
+
+struct ApolloClientOptions {
+  std::string app_id;
+  std::string address; // apollo config server address with port
+  std::string cluster;
+};
+
 struct ConfigMeta {
   std::string app_id;
   std::string cluster{"default"};
   std::string nmspace; // namespace, namespace is a keyword in c++
   std::vector<std::string> keys;
+};
+
+/**
+ * @brief Subscribe meta data.
+ */
+struct SubscribeMeta {
+  std::string nmspace;                  ///< namespace
+  std::unordered_set<std::string> keys; ///< keys should be notified
 };
 } // namespace apollo
