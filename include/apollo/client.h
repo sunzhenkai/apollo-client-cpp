568a6b34759f145da54a496a6352122090b89804
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-18 11:07:07 +0800

    update

--- include/apollo/client.h
+++ include/apollo/client.h
@@ -3,12 +3,18 @@
  * @brief apollo client
  * @author zhenkai.sun
  * */
+#include <functional>
 #include <string>
 #include <unordered_map>
 // self
 #include "apollo/model.h"
 
 namespace apollo {
+/**
+ * @brief notify function for subscribe
+ * */
+using NotifyFunction = std::function<void(Properties &&properties)>;
+
 class ApolloClient {
 public:
   ApolloClient(ApolloClientOptions &&options); //< Construct
@@ -24,7 +30,13 @@ public:
    */
   Properties GetProperties(const std::string &nmspace, int ttl_s = 60);
 
-  void Subscribe(SubscribeMeta &&meta);
+  /**
+   * @brief subscribe a namespace, and callback function will be called when
+   * namespace's properties changed
+   * @param [in] subscribe meta data
+   * @param [in] callback function. function definaltion see @ref NotifyFunction
+   */
+  void Subscribe(SubscribeMeta &&meta, const NotifyFunction &callback);
 
 private:
   ApolloClientOptions options;
