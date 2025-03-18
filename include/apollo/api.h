937a81ac0d60530310fc36ae72105260e0006602
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-18 17:13:36 +0800

    fix compile

--- include/apollo/api.h
+++ include/apollo/api.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "apollo/client.h"
 #include "apollo/model.h"
-##include "apollo/utils.h"
+#include "apollo/utils.h"
 
-    namespace apollo {} // namespace apollo
+namespace apollo {} // namespace apollo
