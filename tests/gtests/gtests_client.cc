21d378fdc0f9830c46720cfe3222b0aea8bfd5e6
zhenkai.sun <zhenkai.sun@qq.com>
2025-05-29 17:53:01 +0800

    add test

--- tests/gtests/gtests_client.cc
+++ tests/gtests/gtests_client.cc
@@ -10,18 +10,19 @@
 
 using std::chrono_literals::operator""s;
 
-apollo::ApolloClientOptions client_options{.app_id = "000111",
+apollo::ApolloClientOptions client_options{.app_id = "apollo-cpp-sdk",
                                            .address = "81.68.181.139:8080",
                                            .cluster = "default",
-                                           .secret_key = "4a46a5e6e6994c1599ddca631f09ecb3"};
+                                           .secret_key = "6360216c46724c16b4dd2a511c8ba3fb"};
 
 TEST(Client, Get) {
   apollo::ApolloClient client(client_options);
   auto r = client.GetProperties("application");
-  spdlog::info("result_size: {}", r.data.size());
+  spdlog::info("result_size: {}", r.GetDataStr());
+  ASSERT_GT(r.data.size(), 0);
 
   r = client.GetProperties("Public");
-  spdlog::info("result_size: {}", r.data.size());
+  spdlog::info("result_size: {}", r.GetDataStr());
   ASSERT_GT(r.data.size(), 0);
 }
 
@@ -29,10 +30,10 @@ TEST(Client, Notify) {
   apollo::ApolloClient client(client_options);
   {
     client.Subscribe({{"application", "Public"}}, [](const std::string &nms, apollo::Properties &&p) {
-      spdlog::info("namespace Properties updated: {}", nms);
+      spdlog::info("namespace Properties updated: {}, Properties: {}", nms, p.GetDataStr());
     });
   }
-  std::this_thread::sleep_for(3s);
+  std::this_thread::sleep_for(15s);
 }
 
 TEST(Client, NotifyV2) {
